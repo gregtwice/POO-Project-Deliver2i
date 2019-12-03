580b3cb4e87a190844bff6dcbd9429f9d1a932ef
Grégoire <gregoire.defoy@ig2i.centralelille.fr>
2019-12-03 17:42:25 +0100

    Implémentation algo trivial

--- src/modele/Solution.java
+++ src/modele/Solution.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,7 +17,7 @@ public class Solution {
     @OneToMany(mappedBy = "appartient")
     private Set<Shift> shifts;
 
-    public Solution() {}
+    public Solution() {this.shifts = new HashSet<>();}
 
     public Long getId() {
         return id;
@@ -25,4 +26,37 @@ public class Solution {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public Instance getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Instance instance) {
+        this.instance = instance;
+    }
+
+    public Set<Shift> getShifts() {
+        return shifts;
+    }
+
+    public void setShifts(Set<Shift> shifts) {
+        this.shifts = shifts;
+    }
+
+
+    public void algoBasique() {
+        for (Tournee tournee : instance.getTournees()) {
+            Shift curShift = new Shift(instance.getDureeMin(), instance.getDureeMax());
+            curShift.addTournee(tournee);
+            shifts.add(curShift);
+        }
+
+        int tempsMort = 0;
+        for (Shift shift : shifts) {
+            tempsMort += shift.tempsMort();
+        }
+        System.out.println(tempsMort);
+
+    }
+
 }
